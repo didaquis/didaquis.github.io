3121f1a7d291b5ca290af852a9494c367b2800a0
Dídac García <didaquis@gmail.com>
2022-11-06 22:08:59 +0100

    Update content

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -2,7 +2,7 @@ const AboutMe = () => (
 	<main>
 		<section>
 			<h2>About me</h2>
-			<p>Hi, my name is Dídac and I'm a full stack web developer living halfway between Barcelona and Terrassa.</p>
+			<p>Hi, my name is Dídac and I'm a full stack web developer living in Barcelona.</p>
 			<p>I’m interested in creating beautiful projects with JavaScript using MERN Stack (MongoDB, ExpressJS, React, NodeJS).</p>
 			<p>Previously, I worked at <a href="https://worksolutions.es" target="_blank" rel="noopener noreferrer" title="WorkSolutions">WorkSolutions</a> and <a href="http://ditec.es/es/" target="_blank" rel="noopener noreferrer" title="Ditec Comunicaciones">Ditec Comunicaciones</a>. Moreover, I studied at <a href="http://www.skylabcoders.com/es" target="_blank" rel="noopener noreferrer" title="Skylab Coders Academy">Skylab Coders Academy</a>. I'm currently working on a cool project at <a href="https://www.promofarma.com" target="_blank" rel="noopener noreferrer" title="PromoFarma">PromoFarma</a>.</p>
 			<p>In my spare time I like improving my skills in Linux and Git or just learn about Node.js, React, GraphQL or anything else. There are so many interesting things in this world, right?</p>
