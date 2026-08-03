367100bb1f181bde693745907051a84b21235ee5
Dídac García <didaquis@gmail.com>
2026-08-03 16:25:04 +0200

    Drop duplicated Node.js mention in About me

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -4,7 +4,7 @@ const AboutMe = () => (
 			<h2>About me</h2>
 			<p>Hi, my name is Dídac and I'm a senior web developer living in the province of Barcelona.</p>
 			<p>I'm interested in creating beautiful projects with TypeScript and Node.js, and in everything happening around AI: agents, skills and automations that help development teams ship faster.</p>
-			<p>In my spare time I like improving my skills in Linux and Git or just learning about Node.js, React, GraphQL or anything else. There are so many interesting things in this world, right?</p>
+			<p>In my spare time I like improving my skills in Linux and Git or just learning about React, GraphQL or anything else. There are so many interesting things in this world, right?</p>
 			<p>When I'm not in front of the computer, I'm probably doing some cool stuff with my Raspberry Pi or I'm out there just rock climbing!<br /><span className="smile-face">¯\_(ツ)_/¯</span></p>
 		</section>
 	</main>
