14f953e8c93bd418adbc63a1888d0111f0a0e217
Dídac García <didaquis@gmail.com>
2026-08-03 16:32:40 +0200

    Rewrite About me spare time paragraphs: AI tinkering, chess and climbing

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -4,8 +4,8 @@ const AboutMe = () => (
 			<h2>About me</h2>
 			<p>Hi, my name is Dídac and I'm a senior web developer living in the province of Barcelona.</p>
 			<p>I'm interested in creating beautiful projects with TypeScript and Node.js, and in everything happening around AI: agents, skills and automations that help development teams ship faster.</p>
-			<p>In my spare time I like improving my skills in Linux and Git or just learning about React, GraphQL or anything else. There are so many interesting things in this world, right?</p>
-			<p>When I'm not in front of the computer, I'm probably doing some cool stuff with my Raspberry Pi or I'm out there just rock climbing!<br /><span className="smile-face">¯\_(ツ)_/¯</span></p>
+			<p>In my spare time I'm usually tinkering with something: an AI agent, a skill, an automation nobody asked for. There are so many interesting things in this world, right?</p>
+			<p>When I'm not in front of the computer, I'm probably playing chess or I'm out there just rock climbing!<br /><span className="smile-face">¯\_(ツ)_/¯</span></p>
 		</section>
 	</main>
 )
