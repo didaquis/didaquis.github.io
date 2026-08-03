e9eb25fd2445991114f5691ee0a026b60ad5b4f8
Dídac García <didaquis@gmail.com>
2026-08-03 16:38:31 +0200

    Restore closing wink in About me spare time paragraph

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -4,7 +4,7 @@ const AboutMe = () => (
 			<h2>About me</h2>
 			<p>Hi, my name is Dídac and I'm a senior web developer living in the province of Barcelona.</p>
 			<p>I'm interested in creating beautiful projects with TypeScript and Node.js, and in everything happening around AI: agents, skills and automations that help development teams ship faster.</p>
-			<p>In my spare time I keep experimenting with AI: building agents, automating my own work and seeing how far they can go.</p>
+			<p>In my spare time I keep experimenting with AI: building agents, automating my own work and seeing how far they can go. We're living in interesting times, right?</p>
 			<p>When I'm not in front of the computer, I'm probably playing chess or I'm out there just rock climbing!<br /><span className="smile-face">¯\_(ツ)_/¯</span></p>
 		</section>
 	</main>
