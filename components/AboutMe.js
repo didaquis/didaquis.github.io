d079bca9825c6cc2507712e5265401d2976ca4b6
Dídac García <didaquis@gmail.com>
2026-08-03 16:21:58 +0200

    Rewrite About me interests around TypeScript, Node.js and AI

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -3,7 +3,7 @@ const AboutMe = () => (
 		<section>
 			<h2>About me</h2>
 			<p>Hi, my name is Dídac and I'm a senior web developer living in the province of Barcelona.</p>
-			<p>I'm interested in creating beautiful projects with JavaScript (React, Vue.js, Node.js, etc).</p>
+			<p>I'm interested in creating beautiful projects with TypeScript and Node.js, and in everything happening around AI: agents, skills and automations that help development teams ship faster.</p>
 			<p>Previously, I worked at <a href="https://worksolutions.es" target="_blank" rel="noopener noreferrer" title="WorkSolutions">WorkSolutions</a>, <a href="https://ditec.es" target="_blank" rel="noopener noreferrer" title="Ditec Comunicaciones">Ditec Comunicaciones</a> and <a href="https://www.promofarma.com" target="_blank" rel="noopener noreferrer" title="PromoFarma">PromoFarma</a>. Moreover, I studied at Skylab Coders Academy, now called <a href="https://isdicoders.com" target="_blank" rel="noopener noreferrer" title="ISDI Coders">ISDI Coders</a>.</p>
 			<p>In my spare time I like improving my skills in Linux and Git or just learning about Node.js, React, GraphQL or anything else. There are so many interesting things in this world, right?</p>
 			<p>When I'm not in front of the computer, I'm probably doing some cool stuff with my Raspberry Pi or I'm out there just rock climbing!<br /><span className="smile-face">¯\_(ツ)_/¯</span></p>
